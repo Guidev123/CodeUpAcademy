1592254ad7dd8f27268e3ceaebdffcda8dd10e81
Guilherme Rafael <155389912+Guidev123@users.noreply.github.com>
2025-02-02 00:01:41 -0300

    feat: implementacao do messagebus

---
+++ src/BuildingBlocks/CodeUp.MessageBus/DependencyInjector.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CodeUp.MessageBus;
+
+public static class DependencyInjector
+{
+    public static void AddMessageBus(this IServiceCollection services, string connection)
+    {
+        if (string.IsNullOrEmpty(connection)) throw new();
+
+        services.AddSingleton<IMessageBus>(new MessageBus(connection));
+    }
+
+    public static string GetMessageQueueConnection(this IConfiguration configuration, string name) =>
+        configuration?.GetSection("MessageQueueConnection")?[name] ?? string.Empty;
+}
---
+++ src/BuildingBlocks/CodeUp.MessageBus/IMessageBus.cs
@@ -0,0 +1,30 @@
+﻿using EasyNetQ;
+
+namespace CodeUp.MessageBus;
+
+public interface IMessageBus : IDisposable
+{
+    bool IsConnected { get; }
+
+    IAdvancedBus AdvancedBus { get; }
+
+    void Publish<T>(T message) where T : class;
+
+    Task PublishAsync<T>(T message) where T : class;
+
+    void Subscribe<T>(string subscriptionId, Action<T> onMessage) where T : class;
+
+    void SubscribeAsync<T>(string subscriptionId, Func<T, Task> onMessage) where T : class;
+
+    TResponse Request<TRequest, TResponse>(TRequest request)
+        where TRequest : class;
+
+    Task<TResponse> RequestAsync<TRequest, TResponse>(TRequest request)
+        where TRequest : class;
+
+    IDisposable Respond<TRequest, TResponse>(Func<TRequest, TResponse> responder)
+        where TRequest : class;
+
+    IDisposable RespondAsync<TRequest, TResponse>(Func<TRequest, Task<TResponse>> responder)
+        where TRequest : class;
+}
