fbbaac7a69aa3d19cd3a91ea2df8b8730726376c
Guilherme Rafael <155389912+Guidev123@users.noreply.github.com>
2025-02-04 00:14:23 -0300

    feat: validacoes do command register

--- src/Modules/Authentication/Modules.Authentication.Application/Commands/Register/RegisterUserValidation.cs
+++ src/Modules/Authentication/Modules.Authentication.Application/Commands/Register/RegisterUserValidation.cs
@@ -2,10 +2,16 @@
 
 namespace Modules.Authentication.Application.Commands.Register;
 
-public class RegisterUserValidation : AbstractValidator<RegisterUserCommand>
+public sealed class RegisterUserValidation : AbstractValidator<RegisterUserCommand>
 {
     public RegisterUserValidation()
     {
+        RuleFor(x => x.FirstName).NotEmpty()
+            .WithMessage("The {PropertyName} field can not be empty")
+            .Length(2, 100).WithMessage("The {PropertyName} field should have 2 - 100 caracters");
 
+        RuleFor(x => x.LastName).NotEmpty()
+            .WithMessage("The {PropertyName} field can not be empty")
+            .Length(2, 100).WithMessage("The {PropertyName} field should have 2 - 100 caracters");
     }
 }
