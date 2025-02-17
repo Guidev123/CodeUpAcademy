b408c1ace76fec7f33b1c42ea9031b4894340002
Guilherme Rafael <155389912+Guidev123@users.noreply.github.com>
2025-02-16 21:25:19 -0300

    refactor

--- src/API/CodeUp.API/Endpoints/Authentication/RefreshTokenEndpoint.cs
+++ src/API/CodeUp.API/Endpoints/Authentication/RefreshTokenEndpoint.cs
@@ -8,11 +8,11 @@ namespace CodeUp.API.Endpoints.Authentication;
 public sealed class RefreshTokenEndpoint : IEndpoint
 {
     public static void Map(IEndpointRouteBuilder app)
-        => app.MapPost("/refresh-token", HandleAsync).Produces<Response<LoginResponseDTO>>().RequireAuthorization();
+        => app.MapPost("/refresh-token/{refreshToken}", HandleAsync).Produces<Response<LoginResponseDTO>>().RequireAuthorization();
 
-    private static async Task<IResult> HandleAsync(IMediator mediator, RefreshTokenCommand command)
+    private static async Task<IResult> HandleAsync(IMediator mediator, string refreshToken)
     {
-        var result = await mediator.Send(command);
+        var result = await mediator.Send(new RefreshTokenCommand(refreshToken));
         return result.IsSuccess ? TypedResults.Created(string.Empty, result) : TypedResults.BadRequest(result);
     }
 }
